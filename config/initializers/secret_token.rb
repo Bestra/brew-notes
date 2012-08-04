1077a293c163335651fe60c9bd2a34ab25228b6c
Chris Westra <cwestra@gmail.com>
2012-08-04 16:51:45 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeerApp::Application.config.secret_token = 'cfe845f3612a9f36ce5ea57b90f80e82c4d13576864e84b1676fec0202723ad552d9dfda79adcf70dd45e87b92da0f0a30f121e7077101db42f5ba68c82ff114'
