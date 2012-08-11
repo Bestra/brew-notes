7fc7a28ad6d5d192071e406d54cd6b3838f629ad
Chris Westra <cwestra@gmail.com>
2012-08-11 17:48:39 -0400

    Add basic user resouce and signup page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,4 @@
 class StaticPagesController < ApplicationController
   def home
   end
-
-  def brew
-  end
-
-  def plan
-  end
-
-  def buy
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 BeerApp::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/brew"
+  resources :users
 
-  get "static_pages/plan"
+  root to: 'static_pages#home'
+  match '/signup', to: 'users#new'
 
-  get "static_pages/buy"
+  match '/home', to: 'static_pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
